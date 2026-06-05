6db213be2354199fa0997fbc11cdb0a374f88a92
xanxxz <boneislandman00@gmail.com>
2026-06-06 00:52:18 +0400

    fix api

--- src/shared/api.ts
+++ src/shared/api.ts
@@ -1,4 +1,4 @@
-const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:5000/api';
+const API_URL = import.meta.env.VITE_API_URL || '/api';
 
 const getToken = () => localStorage.getItem('token');
 
